c7f3c7df67660105d3e2b21de500bf2de39605c8
Hanna922 <hanna.urssu@gmail.com>
2023-04-21 22:08:10 +0900

    style: add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.scss'
+import './styles/Font.css'
 
 function App() {
-  return <div className="App">Hello, I Got It ^~^</div>
+  return <div className="App">아가리 파이터들이 모엿따! Hello, I Got It ^~^</div>
 }
 
 export default App
