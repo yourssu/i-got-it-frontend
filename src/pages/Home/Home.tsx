19bb1f4a680cf13e43454178cf47cfb62e29a24d
Hanna922 <hanna.urssu@gmail.com>
2023-04-23 01:23:46 +0900

    fix(home): css root, home 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,10 +10,12 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <div className="home-title">아가릿!</div>
-      <div className="home-description">{`말로만 다짐하는 사람들을 위한 
+    <div className="home-wrapper">
+      <div className="home-meta">
+        <div className="home-title">아가릿!</div>
+        <div className="home-description">{`말로만 다짐하는 사람들을 위한 
       본격 공개 처형 프로젝트`}</div>
+      </div>
       <div className="recommend"> ↘ 아가리에 다짐을 외쳐보세요 ↙ </div>
       <BoxButton
         type="button"
