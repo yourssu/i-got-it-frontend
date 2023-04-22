eb3a220d070f6183295c729f52b7ac2e72ac90c2
Hanna922 <hanna.urssu@gmail.com>
2023-04-22 11:41:32 +0900

    feat: add routing (App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.scss'
+import Home from './pages/Home'
 import './styles/color.scss'
 import './styles/Font.css'
+import Login from './pages/Login'
 
 function App() {
-  return <div className="App">아가리 파이터들이 모엿따! Hello, I Got It ^~^</div>
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
