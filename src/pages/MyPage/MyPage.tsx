7653c785a88a87548d980bc18973cb8c78fd9ce5
Hanna922 <hanna.urssu@gmail.com>
2023-04-24 23:05:21 +0900

    style(resolution): letter-line 상단 디자인 적용

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -7,20 +7,24 @@ const MyPage = () => {
   return (
     <>
       <MenuHeader onClick={onClickMenu} />
-      <div className="yellow-star">
-        <div className="nickname-resolution">청소기의 “외침”</div>
-        <div className="resolution-line" />
-        <div
-          className="resolution-line"
-          id="resolution-line2"
-        />
-        <div className="resolution">
-          결심들어가는곳 결심들어가는곳 결심들어가는곳 결심들어가는곳💨💨💨
+      <div className="resolution-wrapper">
+        <div className="yellow-star">
+          <div className="nickname-resolution">청소기의 “외침”</div>
+          <div className="resolution-line" />
+          <div
+            className="resolution-line"
+            id="resolution-line2"
+          />
+          <div className="resolution">
+            결심들어가는곳 결심들어가는곳 결심들어가는곳 결심들어가는곳💨💨💨
+          </div>
+          <button
+            type="button"
+            className="envelop"
+          />
         </div>
-        <button
-          type="button"
-          className="envelop"
-        />
+        <div className="d-day">뱉은 말 회수까지</div>
+        <div className="letter-line" />
       </div>
     </>
   )
