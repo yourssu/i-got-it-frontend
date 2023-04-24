012a90a7b48cb7c83a7001bb7ca99084f4b23c27
Hanna922 <hanna.urssu@gmail.com>
2023-04-24 22:51:03 +0900

    style(resolution): 편지봉투 상단까지 디자인 적용

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,4 +1,5 @@
 import MenuHeader from '../../components/Header/MenuHeader'
+import './Mypage.scss'
 
 const onClickMenu = () => {}
 
@@ -6,6 +7,17 @@ const MyPage = () => {
   return (
     <>
       <MenuHeader onClick={onClickMenu} />
+      <div className="yellow-star">
+        <div className="nickname-resolution">청소기의 “외침”</div>
+        <div className="resolution-line" />
+        <div
+          className="resolution-line"
+          id="resolution-line2"
+        />
+        <div className="resolution">
+          결심들어가는곳 결심들어가는곳 결심들어가는곳 결심들어가는곳💨💨💨
+        </div>
+      </div>
     </>
   )
 }
