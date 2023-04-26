a1d48090ad27b1c3dcefd41b7c497385ee8ac959
Minha Ahn <dks4857@gmail.com>
2023-04-26 23:05:40 +0900

    fix : 불필요한 코드 삭제

--- src/pages/atoms/KakaoRedirect.tsx
+++ src/pages/atoms/KakaoRedirect.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
 import { usePostCode } from '../../hooks/usePostCode'
 
 const KakaoRedirect = () => {
@@ -9,7 +8,6 @@ const KakaoRedirect = () => {
     postCode(params.get('code')!)
   }, [])
 
-  // 기존 사용자인지 신규 사용자인지 구분 후 페이지 이동 로직 추가해야함.
   return null
 }
 
