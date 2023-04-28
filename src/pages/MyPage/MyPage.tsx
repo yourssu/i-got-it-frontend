8bb39ee339a3126e62fac26af099c01d99148177
Minha Ahn <dks4857@gmail.com>
2023-04-28 14:37:05 +0900

    feat(resolution) : 버튼 추가

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -4,6 +4,9 @@ import ToastDemo from '../../components/Toast/ToastDemo'
 import MenuContent from '../MenuContent/MenuContent'
 import './Mypage.scss'
 import CheerRelay from '../Cheer/CheerRelay'
+import { useRecoilState } from 'recoil'
+import { userIdState } from '../../State/userIdState'
+import BoxButton from '../../components/Button/BoxButton/BoxButton'
 
 const ToastMessages = {
   ENVELOPE: `뱉은 말은 결심할 때 설정한\n기한 후에 확인 가능해요.`,
@@ -13,6 +16,7 @@ const MyPage = () => {
   const [openToast, setOpenToast] = useState(false)
   const [title, setTitle] = useState('')
   const [openMenu, setOpenMenu] = useState(false)
+  const [userId] = useRecoilState(userIdState)
 
   const onClickMenu = () => {
     setOpenMenu((openMenu) => !openMenu)
@@ -25,6 +29,14 @@ const MyPage = () => {
 
   const onClickEnvelope = () => {
     showToast(ToastMessages.ENVELOPE)
+  }
+
+  const onClickResolution = () => {
+    console.log('나도 결심 외치기')
+  }
+
+  const onClickShareLink = () => {
+    console.log('링크 공유하러 가기')
   }
 
   return (
@@ -61,6 +73,11 @@ const MyPage = () => {
         <div className="letter-line" />
       </div>
       <CheerRelay />
+      <BoxButton
+        text={userId == -1 ? '나도 결심 외치기' : '링크 공유하러 가기'}
+        type="button"
+        onClick={userId == -1 ? onClickResolution : onClickShareLink}
+      />
     </>
   )
 }
