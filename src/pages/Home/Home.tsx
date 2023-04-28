bef76096b362cce3597797221bc8014f08df8c80
Hanna922 <hanna.urssu@gmail.com>
2023-04-28 15:50:26 +0900

    fix(style): add side menu bar in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Home.scss'
 import BoxButton from '../../components/Button/BoxButton'
+import MenuHeader from '../../components/Header/MenuHeader'
+import MenuContent from '../MenuContent/MenuContent'
 
 const Home = () => {
   const navigate = useNavigate()
+  const [openMenu, setOpenMenu] = useState(false)
+
+  const onClickMenu = () => {
+    setOpenMenu((openMenu) => !openMenu)
+  }
+
   const onClick = () => {
     navigate('/create')
   }
 
   return (
-    <div className="home-wrapper">
-      <div className="home-meta">
-        <div className="home-title">아가릿!</div>
-        <div className="home-description">{`말로만 다짐하는 사람들을 위한 
-      본격 공개 처형 프로젝트`}</div>
-      </div>
-      <div className="recommend"> ↘ 아가리에 다짐을 외쳐보세요 ↙ </div>
-      <BoxButton
-        type="button"
-        text="결심하기"
-        onClick={onClick}
+    <>
+      <MenuContent
+        openMenu={openMenu}
+        setOpenMenu={setOpenMenu}
       />
-    </div>
+      <MenuHeader onClick={onClickMenu} />
+      <div className="home-wrapper">
+        <div className="home-meta">
+          <div className="home-title">아가릿!</div>
+          <div className="home-description">{`말로만 다짐하는 사람들을 위한 
+      본격 공개 처형 프로젝트`}</div>
+        </div>
+        <div className="recommend"> ↘ 아가리에 다짐을 외쳐보세요 ↙ </div>
+        <BoxButton
+          type="button"
+          text="결심하기"
+          onClick={onClick}
+        />
+      </div>
+    </>
   )
 }
 
