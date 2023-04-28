825ef12cefe010bcb11a9e4d4e33ebb4f76bcec4
Minha Ahn <dks4857@gmail.com>
2023-04-28 11:40:39 +0900

    feat(addButton) : add 응원 달기 버튼

---
+++ src/components/Button/AddButton/AddButton.tsx
@@ -0,0 +1,29 @@
+//import './BoxButton.scss'
+import Bubble from '../../../images/Cheer/comment_bubble.svg'
+import AddIcon from '../../../images/Cheer/add_icon.svg'
+import './addButton.scss'
+
+interface Iprops {
+  text: string
+  type: 'button' | 'submit' | 'reset' | undefined
+  onClick?: () => void
+  isDisabled?: boolean
+}
+
+const AddButton = () => {
+  return (
+    <div className="add-button">
+      <img
+        className="add-button-icon"
+        src={AddIcon}
+        alt={AddIcon}
+      />
+      <img
+        src={Bubble}
+        alt={Bubble}
+      />
+    </div>
+  )
+}
+
+export default AddButton
