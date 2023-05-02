512f75759ba5201ec3a4fd8d7d36bf0fa1f5f65f
Minha Ahn <dks4857@gmail.com>
2023-05-02 21:57:54 +0900

    fix (delete) : 삭제된 결심일 때 보여주는 화면 수정

--- src/pages/Delete/Delete.tsx
+++ src/pages/Delete/Delete.tsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
 import BoxButton from '@/components/Button/BoxButton/BoxButton'
+import MenuHeader from '@/components/Header/MenuHeader'
+
 import './Delete.scss'
+
+import MenuContent from '../MenuContent/MenuContent'
 
 const Delete = () => {
   const naviagte = useNavigate()
+  const [openMenu, setOpenMenu] = useState(false)
 
   const onClick = () => {
     naviagte('/')
   }
+
+  const onClickMenu = () => {
+    setOpenMenu((openMenu) => !openMenu)
+  }
+
   return (
     <>
+      <MenuContent
+        openMenu={openMenu}
+        setOpenMenu={setOpenMenu}
+      />
+      <MenuHeader
+        onClickMenu={onClickMenu}
+        isDotMenuShow={false}
+        openDotMenu={false}
+      />
       <div className="delete-wrapper">
-        <div className="delete-description">
-          <span>작성자에 의해 삭제된 결심이에요ㅠ</span>
-          <span>이참에 결심의 &quot;주인공&quot;이</span>
-          <span>되어보시겠어요?</span>
+        <div className="delete-description-wrapper">
+          <div className="home-title">아가릿!</div>
+          <div className="delete-description">
+            <span>작성자에 의해 삭제된 결심이에요ㅠ</span>
+            <span>이참에 결심의 &quot;주인공&quot;이</span>
+            <span>되어보시겠어요?</span>
+          </div>
         </div>
         <div className="recommend"> ↘ would you,,? ↙ </div>
       </div>
