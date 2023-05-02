ccb7e1eb7fd9cdd12fd01e100a0b7b59cb7c0b49
Minha Ahn <dks4857@gmail.com>
2023-05-03 00:08:46 +0900

    fix : 결심 상태 초기화 코드 수정

--- src/hooks/useDeleteResolution.ts
+++ src/hooks/useDeleteResolution.ts
@@ -1,17 +1,13 @@
 import { useMutation } from 'react-query'
 import { useNavigate } from 'react-router-dom'
-import { useRecoilState } from 'recoil'
 
 import { deleteResolution } from '@/API/resolution/deleteResolution'
-import { resolutionState } from '@/State/resolutionState'
 
 export const useDeleteResolution = () => {
   const navigate = useNavigate()
-  const [, setResolutionRecoilState] = useRecoilState(resolutionState)
   return useMutation(deleteResolution, {
     onSuccess: () => {
       console.log('success-delete-resolution')
-      setResolutionRecoilState('')
       navigate('/')
     },
     onError: (err) => {
--- src/hooks/usePostResolution.ts
+++ src/hooks/usePostResolution.ts
@@ -3,16 +3,18 @@ import { useNavigate } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 
 import { postResolution } from '@/API/resolution/postResolution'
-import { resolutionIdState } from '@/State/resolutionState'
+import { resolutionIdState, resolutionState } from '@/State/resolutionState'
 
 export const usePostResolution = () => {
   const navigate = useNavigate()
   const [, setResolutionIdState] = useRecoilState(resolutionIdState)
+  const [, setResolutionRecoilState] = useRecoilState(resolutionState)
 
   return useMutation(postResolution, {
     onSuccess: (res) => {
       setResolutionIdState(res.data.resolutionId)
       console.log('success-post-resolution')
+      setResolutionRecoilState('')
       navigate(`/resolutions/${res.data.resolutionId}`)
     },
     onError: (err) => {
