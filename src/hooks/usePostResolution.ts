1a1750aa00e6e5f11bc993ec7f0fb88b068e41bf
dinohan <dinohan.dev@gmail.com>
2023-05-04 00:08:47 +0900

    fix(post-resolution): success 시 letter state 초기화

--- src/hooks/usePostResolution.ts
+++ src/hooks/usePostResolution.ts
@@ -3,18 +3,21 @@ import { useNavigate } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 
 import { postResolution } from '@/API/resolution/postResolution'
+import { letterState } from '@/State/letterState'
 import { resolutionIdState, resolutionState } from '@/State/resolutionState'
 
 export const usePostResolution = () => {
   const navigate = useNavigate()
   const [, setResolutionIdState] = useRecoilState(resolutionIdState)
   const [, setResolutionRecoilState] = useRecoilState(resolutionState)
+  const [, setLetterState] = useRecoilState(letterState)
 
   return useMutation(postResolution, {
     onSuccess: (res) => {
       setResolutionIdState(res.data.resolutionId)
       console.log('success-post-resolution')
       setResolutionRecoilState('')
+      setLetterState('')
       navigate(`/resolutions/${res.data.resolutionId}`)
     },
     onError: (err) => {
